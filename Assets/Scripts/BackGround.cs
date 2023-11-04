59a4311f66af06a0e124ba6550584288e98d1538
informaticaesne <informatica@esne.es>
2023-11-04 17:01:07 +0100

    as

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -6,13 +6,19 @@ public class BackGround : MonoBehaviour
 {
     [SerializeField]
     float backgroundSpeed = 0;
+    [SerializeField]
+    Transform destroyPoint;
     void Start()
     {
-        
     }
 
     void Update()
     {
-        transform.position += new Vector3(transform.position.x * backgroundSpeed * Time.deltaTime,0,0);
+        transform.position += new Vector3(backgroundSpeed * Time.deltaTime,0,0);
+
+        if (transform.position.x <= -23.1900005)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
