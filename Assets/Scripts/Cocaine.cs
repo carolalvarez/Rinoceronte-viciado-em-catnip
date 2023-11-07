b6730a2fcee6be7af38f8f10fb0f06561a537010
Ignacio Tapia <114392929+IgTapia02@users.noreply.github.com>
2023-11-07 20:05:52 +0100

    Esnifar

---
+++ Assets/Scripts/Cocaine.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cocaine : MonoBehaviour
+{
+    float SlideValue= 0;
+    [SerializeField]
+    float SlideMaxValue = 0;
+    [SerializeField]
+    float SlideSume = 0;
+    [SerializeField]
+    Slider slider;
+
+    float temp = 0;
+    void Start()
+    {
+        slider.maxValue = SlideMaxValue;      
+    }
+    void Update()
+    {
+        Debug.Log(SlideValue);
+        slider.value = SlideValue;
+        if (Input.GetMouseButtonDown(1)|| Input.GetMouseButtonDown(0))
+        {
+            if(SlideValue >= SlideMaxValue)
+            {
+                SlideValue = SlideMaxValue;
+            }
+            else if(SlideValue <= SlideMaxValue*0.5)
+            {
+                SlideValue += SlideSume;
+            }
+            else if(SlideValue >= SlideValue*0.5 && SlideValue <= SlideMaxValue*0.33)
+            {
+                SlideValue += SlideSume*0.75f;
+            }
+            else 
+            {
+                SlideValue += SlideSume*0.5f;
+            }
+        }
+        temp += Time.deltaTime;
+        if(temp >= 0.25)
+        {
+            if (SlideValue > 0)
+                SlideValue -= 1;
+            else
+                SlideValue = 0;
+
+            temp = 0;
+        }
+        
+    }
+}
